370ee84b336663f6947063464659242c3053d41a
Jesus <mysteryperutours@gmail.om>
2019-01-12 20:53:50 -0500

    aaaaaaaaaa

--- src/components/widgets/BrandProducts/BrandProducts.jsx
+++ src/components/widgets/BrandProducts/BrandProducts.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { StaticQuery, graphql } from "gatsby";
 import Img from "gatsby-image";
-
 import Media from 'react-media';
  
 
@@ -32,7 +31,10 @@ class BrandProducts extends React.Component {
 
   constructor(props) {
     super(props);
-   
+    if (typeof window !== 'undefined') {
+      const Flickity = require('react-flickity-component');
+      this.state.Flickity = Flickity;
+    }
   }
 
   render() {
@@ -71,11 +73,55 @@ class BrandProducts extends React.Component {
         `}
         render={data => {
 
- 
+ const { Flickity } = this.state;
+
           return(
          <section class="widget-brand-products section">
     <div class="container">
-  
+              <div  className="titulo has-text-centered">
+                <h1 name="image" className="title">
+               Marcas
+                </h1>
+                <h2 className="subtitle">{Titulo}</h2>
+              </div>
+
+  <Flickity
+      className={'carousel'} // default ''
+      elementType={'div'} // default 'div'
+      options={flickityOptions} // takes flickity options {}
+      disableImagesLoaded={false} // default false
+      reloadOnUpdate // default false
+    >
+                      {data.allMarkdownRemark.edges.map((item, i) => {
+                        return (
+                            <div key={i.toString()}>
+                             
+                              <Img 
+                                style={{ width: "160px", margin: "0 auto" }}
+                                sizes={
+                                  item.node.frontmatter.imagen.childImageSharp
+                                    .sizes
+                                }
+                                alt={item.node.frontmatter.title}
+                              />
+                              
+                            </div>
+                        );
+                      })}
+                    </Flickity>
+
+
+
+
+   
+
+
+ 
+
+        
+ 
+          
+               
             </div>
           </section>
         )}}
