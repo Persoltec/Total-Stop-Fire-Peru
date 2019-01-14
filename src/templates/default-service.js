4d685cae0d289a234cb40e4ccf94cfb1ca30d4a6
Jesus <mysteryperutours@gmail.om>
2019-01-14 10:26:57 -0500

    aaaaaaaaaaa

--- src/templates/default-service.js
+++ src/templates/default-service.js
@@ -3,20 +3,32 @@ import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
-
-export const DefaultServiceTemplate = ({ title, content, contentComponent }) => {
+import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
+import Img from "gatsby-image";
+export const DefaultServiceTemplate = ({
+ portada,title, content, contentComponent 
+}) => {
   const PageContent = contentComponent || Content
 
   return (
-      
+    <React.Fragment>
+      <Img
+                          style={{ maxHeight: "300px", margin: "0 auto" }}
+                          sizes={
+                            portada.childImageSharp.sizes
+                          }
+                          alt={title}
+                        />
+                        <br/>
               <PageContent className="content " content={content} />
            
-    
+    </React.Fragment>
   )
 }
 
 
 DefaultServiceTemplate.propTypes = {
+  portada: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   content: PropTypes.string,
   contentComponent: PropTypes.func,
   description: PropTypes.string,
@@ -28,6 +40,7 @@ const DefaultService = ({ data }) => {
   return (
     <Layout titulo={post.frontmatter.title}>
       <DefaultServiceTemplate
+       portada={post.frontmatter.portada}
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
         content={post.html}
