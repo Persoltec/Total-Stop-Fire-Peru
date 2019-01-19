f31c82d00ca1bf3f897edef5a9eecc9034c8f707
Jesus <mysteryperutours@gmail.om>
2019-01-19 00:09:15 -0500

    aaaaaaaaaaaaaa

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -63,6 +63,31 @@ class Footer extends React.Component {
               </div>
             </div>
           
+
+
+ <script
+            dangerouslySetInnerHTML={{
+              __html: `(function () {
+        var options = {
+            facebook: "1381071522167859", // Facebook page ID
+            whatsapp: "987654321", // WhatsApp number
+            call_to_action: "Escribenos", // Call to action
+            button_color: "#E74339", // Color of button
+            position: "right", // Position may be 'right' or 'left'
+            order: "facebook,whatsapp", // Order of buttons
+        };
+        var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();`,
+            }}
+          />
+
+
+
+
+          
         </div>
         </React.Fragment>
     );
